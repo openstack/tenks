609450d2d008a9a29e5780a8c1617bae7635f0fc
Pierre Riteau <pierre@stackhpc.com>
2024-10-28 11:42:56 +0100

    Use openstackdocstheme for release notes
    Change-Id: Ia5868ec45a99d28963adaad89ac6aa4acd729f4b

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -40,7 +40,12 @@ import datetime
 # ones.
 extensions = [
     'reno.sphinxext',
+    'openstackdocstheme',
 ]
+
+openstackdocs_repo_name = 'openstack/tenks'
+openstackdocs_auto_name = False
+openstackdocs_use_storyboard = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -57,10 +62,6 @@ master_doc = 'index'
 # General information about the project.
 project = 'Tenks Release Notes'
 copyright = '{year}, The Tenks team'.format(year=datetime.date.today().year)
-
-openstackdocs_repo_name = 'openstack/tenks'
-openstackdocs_auto_name = False
-openstackdocs_use_storyboard = True
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -112,10 +113,9 @@ pygments_style = 'native'
 
 # -- Options for HTML output ----------------------------------------------
 
-# openstackdocstheme options
-# repository_name = 'openstack/tenks'
-# bug_project = 'tenks'
-# bug_tag = ''
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'openstackdocs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
